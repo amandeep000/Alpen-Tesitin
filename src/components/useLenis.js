499b6412c3b642a83908d91e31936486d8d28370
amandeep000 <deepaman0556@gmail.com>
2025-03-13 21:31:13 +0530

    fixed:lenis smooth scroll

--- src/components/useLenis.js
+++ src/components/useLenis.js
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 
 export function useLenis() {
   useEffect(() => {
     const lenis = new Lenis({
       duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Smooth easing
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smooth: true,
       smoothTouch: true,
     });
