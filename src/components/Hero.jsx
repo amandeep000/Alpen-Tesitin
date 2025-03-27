58bd4d59435facdfbf2c11f34dacc0f79d401c55
amandeep000 <deepaman0556@gmail.com>
2025-03-27 14:18:05 +0530

    fixed bugs in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,8 +6,8 @@ import "swiper/css/effect-fade";
 import Header from "./Header";
 
 function Hero({ isSummer, toggleSeason, isOpen, setIsOpen }) {
-  const summerVideo = "/public/summer/winter bg.mp4";
-  const winterVideo = "/public/winter/summer bg.mp4";
+  const summerVideo = "/summer/winter bg.webm";
+  const winterVideo = "/winter/summer bg.webm";
 
   const videoToShow = isSummer ? summerVideo : winterVideo;
 
@@ -16,7 +16,7 @@ function Hero({ isSummer, toggleSeason, isOpen, setIsOpen }) {
       <div
         className={`${
           isOpen ? "opacity-0" : "opacity-100"
-        } relative w- h-screen overflow-hidden transition-all duration-500 ease-in-out`}
+        } relative w-full h-screen overflow-hidden transition-all duration-500 ease-in-out`}
       >
         <div className="absolute w-full top-0 left-0 z-20">
           <Header
@@ -36,12 +36,20 @@ function Hero({ isSummer, toggleSeason, isOpen, setIsOpen }) {
           autoplay={{ delay: 6000, disableOnInteraction: false }}
           className="w-full h-full"
         >
-          <SwiperSlide>
-            <video className="w-full h-screen object-cover" autoPlay loop muted>
-              <source src={videoToShow} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
-          </SwiperSlide>
+          {/* Duplicate the same video to prevent Swiper errors */}
+          {[1, 2].map((index) => (
+            <SwiperSlide key={`${videoToShow}-${index}`}>
+              <video
+                className="w-full h-screen object-cover"
+                autoPlay
+                loop
+                muted
+              >
+                <source src={videoToShow} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </>
