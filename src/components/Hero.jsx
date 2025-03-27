84457215c3b206a32b9e4715c3151293244474b4
amandeep000 <deepaman0556@gmail.com>
2025-03-27 13:47:19 +0530

    Update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,37 +6,17 @@ import "swiper/css/effect-fade";
 import Header from "./Header";
 
 function Hero({ isSummer, toggleSeason, isOpen, setIsOpen }) {
-  const summerImages = [
-    "/summer/img1.webp",
-    "/summer/img2.webp",
-    "/summer/img3.webp",
-    "/summer/img4.webp",
-    "/summer/img5.webp",
-    "/summer/img6.webp",
-    "/summer/img7.webp",
-    "/summer/img8.webp",
-    "/summer/img9.webp",
-  ];
+  const summerVideo = "/public/summer/winter bg.mp4";
+  const winterVideo = "/public/winter/summer bg.mp4";
 
-  const winterImages = [
-    "/winter/img1.webp",
-    "/winter/img2.webp",
-    "/winter/img3.webp",
-    "/winter/img4.webp",
-    "/winter/img5.webp",
-    "/winter/img6.webp",
-    "/winter/img7.webp",
-    "/winter/img8.webp",
-  ];
-
-  const imagesToShow = isSummer ? summerImages : winterImages;
+  const videoToShow = isSummer ? summerVideo : winterVideo;
 
   return (
     <>
       <div
         className={`${
           isOpen ? "opacity-0" : "opacity-100"
-        } relative w-screen h-screen overflow-x-hidden transition-all duration-500 ease-in-out`}
+        } relative w- h-screen overflow-hidden transition-all duration-500 ease-in-out`}
       >
         <div className="absolute w-full top-0 left-0 z-20">
           <Header
@@ -56,15 +36,12 @@ function Hero({ isSummer, toggleSeason, isOpen, setIsOpen }) {
           autoplay={{ delay: 6000, disableOnInteraction: false }}
           className="w-full h-full"
         >
-          {imagesToShow.map((img, index) => (
-            <SwiperSlide key={index}>
-              <img
-                src={img}
-                className="w-full h-screen object-cover"
-                alt={`Slide ${index + 1}`}
-              />
-            </SwiperSlide>
-          ))}
+          <SwiperSlide>
+            <video className="w-full h-screen object-cover" autoPlay loop muted>
+              <source src={videoToShow} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </SwiperSlide>
         </Swiper>
       </div>
     </>
